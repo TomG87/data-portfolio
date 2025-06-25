73c73b0a36cf30474fb0b80ba71c03ac08be8be1
Tom Gagliano <tgaglianoll@gmail.com>
2025-06-25 18:12:32 -0400

    add header with navigation link

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 const projects = [
   {
-    id:1,
+    id: 1,
     title: "Example",
     description: "Example text",
-    tableauUrl: "tableau website route"
-    sqlQuery: "provide SQL code for query here"
-  }
+    tableauUrl: "tableau website route",
+    sqlQuery: "provide SQL code for query here",
+  },
 ];
 
-app.get('/', (req, res) => {
-  res.render('index', { projects });
+app.get("/", (req, res) => {
+  res.render("index", { projects });
 });
 
-app.get('/projects/:id', (req, res) => {
-  const project = projects.find(p => p.id === parseInt(req.params.id));
-  res.render('project', { project });
+app.get("/projects/:id", (req, res) => {
+  const project = projects.find((p) => p.id === parseInt(req.params.id));
+  res.render("project", { project });
 });
 
 const PORT = process.env.Port || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`);
 });
